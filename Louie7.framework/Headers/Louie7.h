eb551e53cb08e10156fb48f281db2d198391f937
shmuelm <shmuel@louie7.ai>
2022-05-17 13:07:53 +0300

    SDK ios framework for users of louie7ai

---
+++ Louie7.framework/Headers/Louie7.h
@@ -0,0 +1,18 @@
+//
+//  Louie7.h
+//  Louie7
+//
+//  Created by Constantin Breahna on 08.12.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Louie7.
+FOUNDATION_EXPORT double Louie7VersionNumber;
+
+//! Project version string for Louie7.
+FOUNDATION_EXPORT const unsigned char Louie7VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Louie7/PublicHeader.h>
+
+
